9f500ba27fbbf5b44570f8bdd910ff477f2e22cc
sukhiaatma <sawant1810@gmail.com>
2020-05-28 21:49:02 +0530

    updated about and contact page

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
     $('nav .hamburger').on('click', function() {
-        $(this).parent('nav').toggleClass('active');
+        $('body').toggleClass('active');
     });
     var pgName = location.href.split("/").slice(-1),
         pgNames = pgName[0].split(".");
     $('body').attr('data-page', pgNames.length > 1 ? pgNames[0] : 'home');
-    // console.log(123);
+    $('.position').each(function() {
+        if ($(this).outerWidth() < $(this).siblings('.title').outerWidth()) {
+            $(this).css({ width: $(this).siblings('.title').outerWidth() })
+        } else {
+            $(this).siblings('.title').css({ width: $(this).outerWidth() })
+        }
+    }) // console.log(123);
     // var waypoints = $('#footer').waypoint({
     //     handler: function(direction) {
     //         notify(this.element.id + ' hit')
