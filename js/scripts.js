9957cd1660ce04550e76ab881f202458b121de2b
sukhiaatma <sawant1810@gmail.com>
2020-06-02 22:17:05 +0530

    updated code

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,7 @@ $(document).ready(function() {
     var pgName = location.href.split("/").slice(-1),
         pgNames = pgName[0].split(".");
     $('body').attr('data-page', pgNames.length > 1 ? pgNames[0] : 'home');
-    $('.position').each(function() {
-        if ($(this).outerWidth() < $(this).siblings('.title').outerWidth()) {
-            $(this).css({ width: $(this).siblings('.title').outerWidth() }).parents('.name').css({width: $(this).siblings('.title').outerWidth()})
-        } else {
-            $(this).siblings('.title').css({ width: $(this).outerWidth() }).parents('.name').css({width: $(this).outerWidth() })
-        }
-    }) 
+     
     // console.log(123);
     // var waypoints = $('#footer').waypoint({
     //     handler: function(direction) {
@@ -19,4 +13,12 @@ $(document).ready(function() {
     //     }
     // })
 });
-$(window).on('scroll', function() {});
+$(window).on('load', function() {
+    $('.position').each(function() {
+        if ($(this).outerWidth() < $(this).siblings('.title').outerWidth()) {
+            $(this).css({ width: $(this).siblings('.title').outerWidth() }).parents('.name').css({width: $(this).siblings('.title').outerWidth()})
+        } else {
+            $(this).siblings('.title').css({ width: $(this).outerWidth() }).parents('.name').css({width: $(this).outerWidth() })
+        }
+    })
+});
