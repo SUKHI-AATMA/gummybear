a74dda5ca5b2949b593c1f4d0dd64f7ca326a8e2
sukhiaatma <sawant1810@gmail.com>
2020-07-16 16:18:34 +0530

    updated homepage

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,11 @@ $(document).ready(function() {
     });
     var pgName = location.href.split("/").slice(-1),
         pgNames = pgName[0].split(".");
-    $('body').attr('data-page', pgNames.length > 1 ? pgNames[0] : 'home');
-
+    $('body').attr('data-page', (pgNames.length > 1) ? pgNames[0] : 'home');
+    if (pgNames[0] == 'index-new')
+    {
+        $('body').attr('data-page', 'home');
+    }
 
     $('.banner.work').css({height: $(window).outerHeight()})
     // console.log(123);
@@ -42,3 +45,62 @@ function ftrHdrAnim() {
         $('.lftHeader,.rgtHeader').css({ height: '100%' });
     }
 }
+window.addEventListener("scroll", function() { onScrollDiv() });
+window.addEventListener("DOMContentLoaded", function() { setTimeout(function(){onScrollDiv()},0); });
+
+function onScrollDiv() {
+    var images = document.querySelectorAll('.lazyload');
+    var anim = document.querySelectorAll('.animate');
+    for (var i = 0, nb = images.length; i < nb; i++) {
+        var img = images[i];
+        var rect = img.getBoundingClientRect();
+        var isVisible = ((rect.top - window.innerHeight) < 500 && (rect.bottom) > -50) ? true : false;
+
+        if (isVisible) {
+            if (!img.src) {
+                img.src = img.dataset.src;
+                img.removeAttribute('data-src');
+                img.classList.remove('lazyload');
+            }
+        }
+    }
+    // for (var i = 0, nbs = anim.length; i < nbs; i++) {
+    //     var animELe = anim[i];
+    //     var rect = animELe.getBoundingClientRect();
+    //     var isVisible = ((rect.top - window.innerHeight) < 0 && (rect.bottom) > -50) ? true : false;
+
+    //     // console.log(isVisible);
+    //     if (isVisible) {
+    //         if (animELe.hasAttribute('data-anime')) {
+    //             var classL = animELe.getAttribute('data-anime');
+    //             var dataDelay = animELe.getAttribute('data-delay');
+    //             animELe.classList.add(classL);
+    //             animELe.classList.add('delay-'+dataDelay);
+    //             // animELe.removeAttribute('data-anime');
+    //         }
+    //     }
+    // }
+}
+
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    onLeave: function(origin, destination, direction){
+        // console.log(destination.item);
+        (direction == 'down')
+        ? ($(origin.item).removeClass('down up').addClass('up'), $(destination.item).removeClass('down up').addClass('focused')) 
+        : ($(origin.item).removeClass('down up').addClass('down'), $(destination.item).removeClass('down up').addClass('focused'));
+        goo.flow($(origin.item).attr('color'), "out", "up");
+        setTimeout(function(){
+            goo.flow($(destination.item).attr('color'), "in", "down");
+        },500);
+        setTimeout(function(){
+            goo.flow($(destination.item).attr('color'), "out", "up");
+        },900);
+    },
+    afterLoad: function(origin, destination, direction){
+        // console.log(direction);
+        onScrollDiv();
+    },
+});
