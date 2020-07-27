bdf38b33d42de6af284041529440b3c6738660f1
sukhiaatma <sawant1810@gmail.com>
2020-07-27 22:19:21 +0530

    inside pages text animations

--- js/scripts.js
+++ js/scripts.js
@@ -80,69 +80,158 @@ function onScrollDiv() {
     //     }
     // }
 }
+if($('#fullpage').length)
+{
+    new fullpage('#fullpage', {
+        //options here
+        autoScrolling: true,
+        scrollHorizontally: true,
+        onLeave: function(origin, destination, direction) {
+            if (destination.index == 7) {
+                $('.rgtHeader, .lftHeader').animate({ height: $(window).outerHeight(true) - $('footer').outerHeight(true) }, 100, 'linear');
+            } else {
+                $('.rgtHeader, .lftHeader').animate({ height: $(window).outerHeight(true) }, 100, 'linear');
+            }
+            (direction == 'down') ?
+            ($(origin.item).removeClass('down up').addClass('up'), $(destination.item).removeClass('down up').addClass('focused')) :
+            ($(origin.item).removeClass('down up').addClass('down'), $(destination.item).removeClass('down up').addClass('focused'));
+            goo.flow($(origin.item).attr('color'), "out", "up");
+            setTimeout(function() {
+                goo.flow($(destination.item).attr('color'), "in", "down");
+            }, 500);
+            setTimeout(function() {
+                goo.flow($(destination.item).attr('color'), "out", "up");
+            }, 900);
+            $('.section.video.active .close').trigger('click');
+        },
+        afterLoad: function(origin, destination, direction) {
+            console.log(destination.index);
+            onScrollDiv();
+            // console.log($('.fp-completely').hasClass('video'));
+            if (destination.index == 2 || destination.index == 4) {
+                var iframe = document.querySelector('iframe');
+                var player = new Vimeo.Player(iframe);
 
-new fullpage('#fullpage', {
-    //options here
-    autoScrolling: true,
-    scrollHorizontally: true,
-    onLeave: function(origin, destination, direction) {
-        if (destination.index == 7) {
-            $('.rgtHeader, .lftHeader').animate({ height: $(window).outerHeight(true) - $('footer').outerHeight(true) }, 100, 'linear');
-        } else {
-            $('.rgtHeader, .lftHeader').animate({ height: $(window).outerHeight(true) }, 100, 'linear');
-        }
-        (direction == 'down') ?
-        ($(origin.item).removeClass('down up').addClass('up'), $(destination.item).removeClass('down up').addClass('focused')) :
-        ($(origin.item).removeClass('down up').addClass('down'), $(destination.item).removeClass('down up').addClass('focused'));
-        goo.flow($(origin.item).attr('color'), "out", "up");
-        setTimeout(function() {
-            goo.flow($(destination.item).attr('color'), "in", "down");
-        }, 500);
-        setTimeout(function() {
-            goo.flow($(destination.item).attr('color'), "out", "up");
-        }, 900);
-        $('.section.video.active .close').trigger('click');
-    },
-    afterLoad: function(origin, destination, direction) {
-        console.log(destination.index);
-        onScrollDiv();
-        // console.log($('.fp-completely').hasClass('video'));
-        if (destination.index == 2 || destination.index == 4) {
-            var iframe = document.querySelector('iframe');
-            var player = new Vimeo.Player(iframe);
-
-            $('.section.video.active .prjTitle').animate({ opacity: 0 }, 300, '', function() {
-                setTimeout(function() {
-                    $('.section.video.active .prjTitle').animate({ width: 0, padding: 0 }, 500);
-                    $('.section.video.active .prjImg').css({ width: '100%', position: 'sticky', top: 0, left: 0, height: $(window).height() }).find('video').css({width:'60%'});
-                    $('.grain').fadeOut(300);
-                    $('.lftHeader').animate({ left: '-10%' }, 300);
-                    $('.rgtHeader').animate({ right: '-10%' }, 300);
-                    $('section.fpage').animate({ margin: '0%', width: '100%' }, 300);
-                    $('.section.video.active .close').fadeIn(300);
+                $('.section.video.active .prjTitle').animate({ opacity: 0 }, 300, '', function() {
+                    setTimeout(function() {
+                        $('.section.video.active .prjTitle').animate({ width: 0, padding: 0 }, 500);
+                        $('.section.video.active .prjImg').css({ width: '100%', position: 'sticky', top: 0, left: 0, height: $(window).height() }).find('video').css({width:'60%'});
+                        $('.grain').fadeOut(300);
+                        $('.lftHeader').animate({ left: '-10%' }, 300);
+                        $('.rgtHeader').animate({ right: '-10%' }, 300);
+                        $('section.fpage').animate({ margin: '0%', width: '100%' }, 300);
+                        $('.section.video.active .close').fadeIn(300);
+                        if(destination.index == 2)
+                        {
+                            player.play();
+                        }
+                    }, 500)
+                });
+                $('.section.video.active .close').on('click', function() {
                     if(destination.index == 2)
                     {
-                        player.play();
+                        player.pause();
                     }
-                }, 500)
-            });
-            $('.section.video.active .close').on('click', function() {
-                if(destination.index == 2)
-                {
-                    player.pause();
-                }
-                $('.section.video.active .close').fadeOut(300);
-                $('section.fpage').animate({ margin: '0 8% 0 10%', width: '82%' }, 300);
-                $('.rgtHeader').animate({ right: '0' }, 300);
-                $('.lftHeader').animate({ left: '0' }, 300);
-                $('.section.video.active .prjImg').css({ width: '60%', position: 'static', top: 0, left: 0, height: 'unset' }).find('video').css({width:'160%'});
-                $('.section.video.active .prjTitle').animate({ width: '40%', padding: '0 0 0 5%' }, 500);
-                $('.section.video.active .prjTitle').animate({ opacity: 1 }, 300);
-                $('.grain').fadeIn(300);
-            });
-            // var lft = $('.section.video.active').offset().left - $('.section.video.active').find('.prjImg').position().left;
-            // var lft = $(window).width() - $('.section.video.active').find('.prjImg').position().left - $('.section.video.active .prjImg iframe').outerWidth(true);
-            // $('.section.video.active').find('iframe').css({ width: $('.section.video.active').outerWidth(true), height: $('.section.video.active').outerHeight(true), top: 0, left: lft })
-        }
-    },
+                    $('.section.video.active .close').fadeOut(300);
+                    $('section.fpage').animate({ margin: '0 8% 0 10%', width: '82%' }, 300);
+                    $('.rgtHeader').animate({ right: '0' }, 300);
+                    $('.lftHeader').animate({ left: '0' }, 300);
+                    $('.section.video.active .prjImg').css({ width: '60%', position: 'static', top: 0, left: 0, height: 'unset' }).find('video').css({width:'160%'});
+                    $('.section.video.active .prjTitle').animate({ width: '40%', padding: '0 0 0 5%' }, 500);
+                    $('.section.video.active .prjTitle').animate({ opacity: 1 }, 300);
+                    $('.grain').fadeIn(300);
+                });
+                // var lft = $('.section.video.active').offset().left - $('.section.video.active').find('.prjImg').position().left;
+                // var lft = $(window).width() - $('.section.video.active').find('.prjImg').position().left - $('.section.video.active .prjImg iframe').outerWidth(true);
+                // $('.section.video.active').find('iframe').css({ width: $('.section.video.active').outerWidth(true), height: $('.section.video.active').outerHeight(true), top: 0, left: lft })
+            }
+        },
+    });
+}
+// if($('body').attr('data-page') != 'home')
+// {
+//     var lastScrollTop = 0;
+//     // $('.title, .text').addClass('upTxt');
+//         var cls;
+//     $(window).scroll(function(event) {
+//         var st = $(this).scrollTop();
+//         if (st > lastScrollTop) {
+//             // downscroll code
+//             cls= "upTxt";
+//         } else {
+//             cls= "downTxt";
+//             // upscroll code
+//         }
+//         lastScrollTop = st;
+//     });
+//     $('.title').on('scrollSpy:enter', function() {
+//         console.log($(this).text());
+//         $(this).removeClass('upTxt').removeClass('downTxt').addClass('focusedTxt');
+//     });
+//     $('.title').on('scrollSpy:exit', function() {
+//         console.log($(this).text());
+//         $(this).addClass(cls);
+//     });
+// }
+
+$(window).on('scroll',function(){
+    animateText();
+}).on('load',function(){
+    animateText();
 });
+function animateText() {
+    var anim = document.querySelectorAll('.title');
+    var animP = document.querySelectorAll('.pageSubTitle');
+    var animT = document.querySelectorAll('.text');
+    for (var i = 0, nbs = anim.length; i < nbs; i++) {
+        var animELe = anim[i];
+        var rect = animELe.getBoundingClientRect();
+        var isVisible = ((rect.top - window.innerHeight) < 0 && (rect.bottom) > -50) ? true : false;
+
+        // console.log(isVisible);
+        if (isVisible) {
+            // if (animELe.hasAttribute('data-anime')) {
+                // var classL = animELe.getAttribute('data-anime');
+                // var dataDelay = animELe.getAttribute('data-delay');
+                animELe.classList.remove('upTxt');
+                animELe.classList.remove('downTxt');
+                // animELe.classList.add('focusedTxt');
+                // animELe.removeAttribute('data-anime');
+            // }
+        }
+    }
+    for (var i = 0, nbs = animT.length; i < nbs; i++) {
+        var animELe = animT[i];
+        var rect = animELe.getBoundingClientRect();
+        var isVisible = ((rect.top - window.innerHeight) < 0 && (rect.bottom) > -50) ? true : false;
+
+        // console.log(isVisible);
+        if (isVisible) {
+            // if (animELe.hasAttribute('data-anime')) {
+                // var classL = animELe.getAttribute('data-anime');
+                // var dataDelay = animELe.getAttribute('data-delay');
+                animELe.classList.remove('upTxt');
+                animELe.classList.remove('downTxt');
+                // animELe.classList.add('focusedTxt');
+                // animELe.removeAttribute('data-anime');
+            // }
+        }
+    }
+    for (var i = 0, nbs = animP.length; i < nbs; i++) {
+        var animELe = animP[i];
+        var rect = animELe.getBoundingClientRect();
+        var isVisible = ((rect.top - window.innerHeight) < 0 && (rect.bottom) > -50) ? true : false;
+
+        // console.log(isVisible);
+        if (isVisible) {
+            // if (animELe.hasAttribute('data-anime')) {
+                // var classL = animELe.getAttribute('data-anime');
+                // var dataDelay = animELe.getAttribute('data-delay');
+                animELe.classList.remove('upTxt');
+                animELe.classList.remove('downTxt');
+                // animELe.classList.add('focusedTxt');
+                // animELe.removeAttribute('data-anime');
+            // }
+        }
+    }
+}
